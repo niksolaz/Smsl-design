dc5dd468618318213ffe1586403ff455718cbcfd
niksolaz <solazzo.nicola@gmail.com>
2015-09-29 18:16:23 +0200

    added Header route to navigate link

--- components/Root.jsx
+++ components/Root.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
+var Header = require('./Header.jsx');
 
 var Root = React.createClass({
   render: function () {

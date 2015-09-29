908f90fb9b840a5ae8ce9243be50efbd524f54d1
niksolaz <solazzo.nicola@gmail.com>
2015-09-29 18:13:19 +0200

    added dangerouslySetInnerHTML in Root

--- components/Root.jsx
+++ components/Root.jsx
@@ -4,6 +4,9 @@ var RouteHandler = Router.RouteHandler;
 
 var Root = React.createClass({
   render: function () {
+    var initialProps = {
+      __html: safeStringify(this.props)
+    }
     return (
       <html>
         <head>
@@ -11,10 +14,19 @@ var Root = React.createClass({
         </head>
         <body>
           <RouteHandler {...this.props} />
+          <script
+            id='initial-props'
+            type='application/json'
+            dangerouslySetInnerHTML={initialProps} />
+          <script src='bundle.js' />
         </body>
       </html>
     );
+    
   }
 });
 
+function safeStringify(obj) {
+  return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--');
+};
 module.exports = Root;
